242e99e7ec663a1bca709f3c11239fae138568ac
whals0016 <whals0016@naver.com>
2021-08-27 17:29:52 +0900

    과제 완료 커밋

---
+++ Hotel_JMJTest2/src/main/java/com/human/app/HomeController.java
@@ -0,0 +1,143 @@
+package com.human.app;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private HttpSession session;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home"; //.jsp 생략
+	}
+	
+	@RequestMapping("home")
+	public String home1() {
+		return "home"; //.jsp 생략
+	}
+	
+	@RequestMapping("newbie")
+	public String newbie() {
+		return "newbie"; //.jsp 생략
+	}
+	
+	@RequestMapping("/Login")
+	public String Login() {
+		return "Login"; //.jsp 생략
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest hsr) {
+		HttpSession session=hsr.getSession();
+		session.invalidate(); //세션값을 초기화
+		return "redirect:/"; //.jsp 생략
+	}
+	
+	@RequestMapping("/room")
+	public String room(HttpServletRequest hsr) {
+		HttpSession session=hsr.getSession();
+		if(session.getAttribute("loginid")==null) {
+			return "redirect:login"; //.jsp 생략
+		}
+		return "room"; //.jsp 생략
+	}
+	
+	
+	@RequestMapping(value="/check_user",method=RequestMethod.POST)
+	public String check_user(HttpServletRequest hsr, Model model) {
+		String userid=hsr.getParameter("userid");
+		String passcode=hsr.getParameter("passcode");
+		
+		HttpSession session=hsr.getSession();
+		session.setAttribute("loginid",userid);
+		
+		return "redirect://booking"; //RequestMapping의 경로이름
+	}
+	
+	@RequestMapping(value="/booking",method=RequestMethod.GET)
+	public String booking1(HttpServletRequest hsr) {
+		HttpSession session = hsr.getSession();
+		session.setAttribute("nickname", "aud");
+		return "booking"; //jsp화일 이름
+	}
+	
+	/*
+	@RequestMapping(value="/check_user",method=RequestMethod.GET)
+	public String booking(HttpServletRequest hsr) {
+		HttpSession session = hsr.getSession();
+		String loginid=(String)session.getAttribute("userid");
+		if(!loginid.equals("aud")) {
+			return "booking"; //.jsp 생략
+		} else {
+			return "redirect:/"; //JSP화일이름
+		}
+	}
+	*/
+	
+	@RequestMapping("/selected")
+	public String doJob(HttpServletRequest hsr,Model model) {
+	String strPath=hsr.getParameter("path");
+		if(strPath.equals("Login")) {
+			return "Login"; //.jsp 생략
+		} else if(strPath.equals("newbie")) {
+			return "newbie"; //.jsp 생략
+		} else {
+			return "redirect:home"; //redirect:요청경로명
+		}
+	}
+	
+	@RequestMapping("/newinfo")
+	public String doInfo(HttpServletRequest hsr, Model model) {
+		String una=hsr.getParameter("username");
+		String uid=hsr.getParameter("userid");
+		String pw=hsr.getParameter("passcode");
+		String pw2=hsr.getParameter("passcode2");
+		String mb=hsr.getParameter("mobile");
+		
+//		System.out.println("usename="+una);
+//		System.out.println("userid="+uid);
+//		System.out.println("passcode="+pw);
+//		System.out.println("passcode2="+pw2);
+//		System.out.println("mobile="+mb);
+		
+		model.addAttribute("username",una);
+		model.addAttribute("userid",uid);
+		model.addAttribute("passcode",pw);
+		model.addAttribute("passcode2",pw2);
+		model.addAttribute("mobile",mb);
+		return "newinfo"; //.jsp 생략
+	}
+	
+	@RequestMapping("/viewinfo")
+	public String doInfo2(HttpServletRequest hsr2, Model model) {
+		String uid=hsr2.getParameter("userid");
+		String pw=hsr2.getParameter("passcode");
+		
+//		System.out.println("userid="+uid);
+//		System.out.println("password="+pw);
+		
+		model.addAttribute("userid",uid);
+		model.addAttribute("passcode",pw);
+		return "viewinfo"; //.jsp 생략
+	}
+	
+}
---
+++ Hotel_JMJTest2/src/main/webapp/WEB-INF/views/newbie.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원가입</title>
+</head>
+<body>
+<form method="POST" action="/app/home">
+	이름: <input type="text" name=username><br>
+	로긴아이디: <input type="text" name=userid><br>
+	비밀번호: <input type="password" name=passcode><br>
+	비밀번호 확인: <input type="password" name=passcode2><br>
+	모바일: <input type="text" name=mobile><br>
+	<input type=submit value="등록" location.href='home'>
+</form>
+<button onclick="location.href='home'">취소</button>
+</body>
+</html>
